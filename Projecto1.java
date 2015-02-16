85862da619de2fc99bac1504a1149c1cc96e64da
Miguel Duarte <miguel.duarte20@hotmail.com>
2015-02-16 16:35:46 +0000

    Added project 1 code file

---
+++ Projecto1.java
@@ -0,0 +1,17 @@
+public class Projecto1{
+
+
+	public Projecto1(){
+
+	}
+	public static void main(String[] args){
+		new Projecto1()
+	}
+}
+
+public class Cell{
+	int grass;
+	public Cell(){
+		
+	}
+}
