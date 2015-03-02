b9fd87c1ffc6afc2d0b1c2bfbfa23daaa7523d55
Miguel Duarte <miguel.duarte20@hotmail.com>
2015-03-02 18:35:55 +0000

    Added Project 2 code and EventS lib

---
+++ Projecto2/Projecto2.java
@@ -0,0 +1,20 @@
+import eventS.*;
+
+public class Projecto2{
+	public Projecto2(){
+		//Model model = new Server(1);
+		/*MyModel model = new MyModel(1);
+		Simulator simulator = new Simulator(model);
+		model.simulator(simulator);
+		simulator.run();*/
+	}
+	public static void main(String args[]){
+		new Projecto2();
+	}
+}
+/*
+class MyModel extends Model{
+	private void init(){
+
+	}
+}*/
