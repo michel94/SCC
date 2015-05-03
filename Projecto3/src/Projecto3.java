285d5d34405fa999720c2cad97bf3d77ca219a6f
Guilherme Graca <ggracase@gmail.com>
2015-05-03 18:35:30 +0100

    Project3 environment created

---
+++ Projecto3/src/Projecto3.java
@@ -0,0 +1,21 @@
+import desmoj.core.simulator.*;
+
+public class Projecto3{
+    public static void main (String[] args) {
+        //create model and experiment
+        MyModelClass model = new MyModelClass();
+        Experiment exp = new Experiment("MyExperiment");
+        //and link them
+        model.connectToExperiment(exp);
+        //initialize experiments parameters
+        exp.setShowProgressBar(true);
+        exp.tracePeriod(new TimeInstant(0.0), new TimeInstant(100.0));
+        exp.debugPeriod(new TimeInstant(10.0), new TimeInstant(50.0));
+        exp.stop(new TimeInstant(480.0));
+        //start experiment
+        exp.start();
+        //generate report and shut everything down
+        exp.report();
+        exp.finish();
+    }
+}
