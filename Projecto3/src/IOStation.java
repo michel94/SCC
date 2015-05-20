6ad07953b74ff0cb12fb38738c69dd300e13e47b
Guilherme Graca <ggracase@gmail.com>
2015-05-20 01:48:08 +0100

    Fixed Job Dist Bug

--- Projecto3/src/IOStation.java
+++ Projecto3/src/IOStation.java
@@ -5,18 +5,19 @@ import java.util.concurrent.*;
 public class IOStation extends SimProcess{
 	MainModel model;
 	Station ioStation;
-	ContDistConstant jobTypeDist;
+	ContDistUniform jobTypeDist;
 
 	public IOStation(MainModel model){
 		super(model, "IOStation", false, false);
 		this.model = model;
-		
-		jobTypeDist = new ContDistConstant(model, "jobTypeDist", 1, false, false);
+
+		jobTypeDist = new ContDistUniform(model, "jobTypeDist", 0, 1, false, false);
 		System.out.println("jobTypeDist");
 	}
 	public void init(){
 	}
 	public void lifeCycle(){
+		System.out.println("lifeCycle");
 		while(true){
 
 			int type;
@@ -31,11 +32,10 @@ public class IOStation extends SimProcess{
 			Job job = new Job(model, type);
 
 			Double t = model.newJobDistTime();
-			System.out.println("New Job " + t);
+			//System.out.println("New Job " + t+ " type: " + type);
 			hold(new TimeSpan(t, TimeUnit.MINUTES));
-			
-			model.getAvg().pushToQueue(job);
 
+			model.getAvg().pushToQueue(job);
 		}
 	}
 }
