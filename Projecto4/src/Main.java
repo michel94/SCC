00e8e8472257c1760a0d0cf0427e6535402f2592
Miguel Martins Duarte <miguel.duarte20@hotmail.com>
2015-06-09 21:55:29 +0100

    2 and 3 serial level tests

--- Projecto4/src/Main.java
+++ Projecto4/src/Main.java
@@ -28,13 +28,66 @@ public class Main{
     System.out.println("Kolmogorov: " + minDiff + " " + maxDiff);
   }
 
-  public void TwoLevelTest(double d[]){
+  public double coisa(int k){
+    return 2.0 / (9.0 * k);
+  }
+
+  public double crit(double value){
+    return -1.645;
+  }
+
+  public double chiSquareDist(int k, double alpha){
+    return k * Math.pow( 1 - coisa(k) + crit(1-alpha) * Math.sqrt(coisa(k)), 3);
+  }
+
+  public double chiSquareTest(double data[], double e){
+    double sum=0;
+    for(int i=0; i<data.length; i++){
+      sum += Math.pow(data[i] - e, 2) / e;
+    }
+
+    return sum;
+
+  }
+
+  public double TwoLevelTest(double d[]){
     double data[] = Arrays.copyOf(d, d.length);
     int quads[][] = new int[k][k];
     for(int i=0; i<n; i+=2){
-      quads[round(data[i]*k)][round(data[i+1]*k)]++;
+      quads[(int)(data[i]*k) ][(int)(data[i+1]*k) ]++;
     }
+    double count[] = new double[k * k];
+    for(int i=0; i<k; i++){
+      for(int j=0; j<k; j++){
+        count[i + j*k] = quads[i][j];
+      }
+    }
+    double result = chiSquareTest(count, (n/2.0) / (k*k) );
+    double limit = chiSquareDist(k*k-1, 1-0.05);
+    System.out.println("TwoLevelTest: " + result + " " + limit);
 
+    return result;
+  }
+
+  public double ThreeLevelTest(double d[]){
+    double data[] = Arrays.copyOf(d, d.length);
+    int quads[][][] = new int[k][k][k];
+    for(int i=0; i<n; i+=3){
+      quads[(int)(data[i]*k) ][(int)(data[i+1]*k) ][(int)(data[i+2]*k) ]++;
+    }
+    double count[] = new double[k*k*k];
+    for(int i=0; i<k; i++){
+      for(int j=0; j<k; j++){
+        for(int l=0; l<k; l++){
+          count[i + j*k + l*k*k] = quads[i][j][l];
+        }
+      }
+    }
+    double result = chiSquareTest(count, (n/3.0) / (k*k*k) );
+    double limit = chiSquareDist(k*k*k-1, 1-0.05);
+    System.out.println("ThreeLevelTest: " + result + " " + limit);
+
+    return result;
   }
 
   public Main(){
@@ -52,9 +105,13 @@ public class Main{
     }
 
     KSTest(data);
+    TwoLevelTest(data);
+    ThreeLevelTest(data);
 
-
-
+    /*double trTest = TwoLevelTest(data);
+    double limit = chiSquare(30*30-1, 1-0.05);
+    System.out.println("TwoLevelTest " + trTest + " " + limit);
+*/
   }
 
   public static void main(String[] args) {
